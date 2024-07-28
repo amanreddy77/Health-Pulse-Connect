d8ba65696d4cf0212425efc350cd023706582951
amanreddy77 <reddyaman77.ar@gmail.com>
2024-07-28 23:16:53 +0530

    few updates

--- src/Redux/Reducers/childReducer.js
+++ src/Redux/Reducers/childReducer.js
@@ -7,7 +7,7 @@ const initialState = {
     loading: false,
     error: null,
 };
-
+// fetching
 const childReducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_CHILDREN_REQUEST:
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,12 @@
-
-import {createStore,combineReducers, applyMiddleware} from 'redux';
-import {thunk} from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk'
 import { composeWithDevTools } from '@redux-devtools/extension';
 import childReducer from './Reducers/childReducer';
 
 const reducer = combineReducers({
-    getchilds: childReducer
-})
-
+        getchilds: childReducer
+    })
+    // middleware
 const middleware = [thunk];
 const store = createStore(
     reducer,
